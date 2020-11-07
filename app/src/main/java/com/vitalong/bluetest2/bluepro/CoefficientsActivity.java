ce4bf07d25cd214496c277142b6cdbfa16d75eb9
perfectliang <332685954@qq.com>
2020-11-07 17:18:53 +0800

    1.每个矫正参数添加单个设置功能 2. 避免错位读取的问题及某个是空的，可能是重复点击按钮，在读取板子数据的时候不能再点击

--- app/src/main/java/com/vitalong/bluetest2/bluepro/CoefficientsActivity.java
+++ app/src/main/java/com/vitalong/bluetest2/bluepro/CoefficientsActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -59,20 +60,28 @@ public class CoefficientsActivity extends MyBaseActivity2 {
     public ImageView imgBxisC;
     //    @Bind(R.id.imgBxisD)
     public ImageView imgBxisD;
+    public TextView tvAaxisA;
+    public TextView tvAaxisB;
+    public TextView tvAaxisC;
+    public TextView tvAaxisD;
+    public TextView tvBaxisA;
+    public TextView tvBaxisB;
+    public TextView tvBaxisC;
+    public TextView tvBaxisD;
     public Button btnRefresh;
     public TextView tvCheck;
     private VerifyDataBean verifyDataBean;
     private CoefficientsHandler coefficientsHandler;
     private int count = 0;
     private int refreshCount = 0;
+    private int singleCount = 0;
     private long sendTime = 0;
     private long receiveTime = 0;
     private long DELAYTIME = 500; //设置矫正参数超时时间
     private long SEND_DURATION = 500;//发送设置命令的时间间隔为500ms
     int delayTime = 400;
     private RefreshHandler refreshHandler;
-    private boolean isRefresh = false;
-
+    private int isRefresh = 0; //0是刷新 1是全部重新設置  2是单个设置
     private String orginal1Str = "";
     private String orginal2Str = "";
     private String orginal3Str = "";
@@ -116,6 +125,14 @@ public class CoefficientsActivity extends MyBaseActivity2 {
         imgBxisC = findViewById(R.id.imgBxisC);
         imgBxisD = findViewById(R.id.imgBxisD);
         tvCheck = findViewById(R.id.check);
+        tvAaxisA = findViewById(R.id.tvAaxisA);
+        tvAaxisB = findViewById(R.id.tvAaxisB);
+        tvAaxisC = findViewById(R.id.tvAaxisC);
+        tvAaxisD = findViewById(R.id.tvAaxisD);
+        tvBaxisA = findViewById(R.id.tvBaxisA);
+        tvBaxisB = findViewById(R.id.tvBaxisB);
+        tvBaxisC = findViewById(R.id.tvBaxisC);
+        tvBaxisD = findViewById(R.id.tvBaxisD);
     }
 
     private void initData() {
@@ -141,7 +158,8 @@ public class CoefficientsActivity extends MyBaseActivity2 {
                 if (verify()) {
                     clearUIState();
                     //这个1不是时间
-                    isRefresh = true;
+                    btnSet.setEnabled(false);
+                    isRefresh = 1;
                     coefficientsHandler.sendEmptyMessage(1);
                 }
             }
@@ -151,8 +169,9 @@ public class CoefficientsActivity extends MyBaseActivity2 {
 
             @Override
             public void onClick(View v) {
-                isRefresh = false;
+                isRefresh = 0;
                 clearEdtData();
+                btnRefresh.setEnabled(false);
                 refreshHandler.sendEmptyMessageDelayed(1, delayTime);
             }
         });
@@ -170,6 +189,134 @@ public class CoefficientsActivity extends MyBaseActivity2 {
                 i.putExtra("orginal7Str", orginal7Str);
                 i.putExtra("orginal8Str", orginal8Str);
                 startActivity(i);
+            }
+        });
+
+        tvAaxisA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edtAxiasA.getText().toString().isEmpty()) {
+                    Toast.makeText(CoefficientsActivity.this, "AaxisA的值不能為空", Toast.LENGTH_SHORT).show();
+                } else {
+                    isRefresh = 2;
+                    singleCount = 1;
+                    String cmd = edtAxiasA.getText().toString();
+                    cmd = cmdAnalyze.getParamToCmd(cmd);
+                    cmd = cmdClass.getParamsCmd(cmd, String.valueOf(0));//getParamsCmd
+                    sendCmdCodeByHex(cmd);
+                }
+            }
+        });
+
+        tvAaxisB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edtAxiasB.getText().toString().isEmpty()) {
+                    Toast.makeText(CoefficientsActivity.this, "AaxisA的值不能為空", Toast.LENGTH_SHORT).show();
+                } else {
+                    isRefresh = 2;
+                    singleCount = 2;
+                    String cmd = edtAxiasB.getText().toString();
+                    cmd = cmdAnalyze.getParamToCmd(cmd);
+                    cmd = cmdClass.getParamsCmd(cmd, String.valueOf(1));//getParamsCmd
+                    sendCmdCodeByHex(cmd);
+                }
+            }
+        });
+
+        tvAaxisC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edtAxiasC.getText().toString().isEmpty()) {
+                    Toast.makeText(CoefficientsActivity.this, "AaxisA的值不能為空", Toast.LENGTH_SHORT).show();
+                } else {
+                    isRefresh = 2;
+                    singleCount = 3;
+                    String cmd = edtAxiasC.getText().toString();
+                    cmd = cmdAnalyze.getParamToCmd(cmd);
+                    cmd = cmdClass.getParamsCmd(cmd, String.valueOf(2));//getParamsCmd
+                    sendCmdCodeByHex(cmd);
+                }
+            }
+        });
+
+        tvAaxisD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edtAxiasD.getText().toString().isEmpty()) {
+                    Toast.makeText(CoefficientsActivity.this, "AaxisA的值不能為空", Toast.LENGTH_SHORT).show();
+                } else {
+                    isRefresh = 2;
+                    singleCount = 4;
+                    String cmd = edtAxiasD.getText().toString();
+                    cmd = cmdAnalyze.getParamToCmd(cmd);
+                    cmd = cmdClass.getParamsCmd(cmd, String.valueOf(3));//getParamsCmd
+                    sendCmdCodeByHex(cmd);
+                }
+            }
+        });
+
+        tvBaxisA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edtBxiasA.getText().toString().isEmpty()) {
+                    Toast.makeText(CoefficientsActivity.this, "AaxisA的值不能為空", Toast.LENGTH_SHORT).show();
+                } else {
+                    isRefresh = 2;
+                    singleCount = 5;
+                    String cmd = edtBxiasA.getText().toString();
+                    cmd = cmdAnalyze.getParamToCmd(cmd);
+                    cmd = cmdClass.getParamsCmd(cmd, String.valueOf(4));//getParamsCmd
+                    sendCmdCodeByHex(cmd);
+                }
+            }
+        });
+
+        tvBaxisB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edtBxiasB.getText().toString().isEmpty()) {
+                    Toast.makeText(CoefficientsActivity.this, "AaxisA的值不能為空", Toast.LENGTH_SHORT).show();
+                } else {
+                    isRefresh = 2;
+                    singleCount = 6;
+                    String cmd = edtBxiasB.getText().toString();
+                    cmd = cmdAnalyze.getParamToCmd(cmd);
+                    cmd = cmdClass.getParamsCmd(cmd, String.valueOf(5));//getParamsCmd
+                    sendCmdCodeByHex(cmd);
+                }
+            }
+        });
+
+        tvBaxisC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edtBxiasC.getText().toString().isEmpty()) {
+                    Toast.makeText(CoefficientsActivity.this, "AaxisA的值不能為空", Toast.LENGTH_SHORT).show();
+                } else {
+                    isRefresh = 2;
+                    singleCount = 7;
+                    String cmd = edtBxiasC.getText().toString();
+                    cmd = cmdAnalyze.getParamToCmd(cmd);
+                    cmd = cmdClass.getParamsCmd(cmd, String.valueOf(6));//getParamsCmd
+                    sendCmdCodeByHex(cmd);
+                }
+            }
+        });
+
+        tvBaxisD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edtBxiasD.getText().toString().isEmpty()) {
+                    Toast.makeText(CoefficientsActivity.this, "AaxisA的值不能為空", Toast.LENGTH_SHORT).show();
+                } else {
+                    isRefresh = 2;
+                    singleCount = 8;
+                    String cmd = edtBxiasD.getText().toString();
+                    cmd = cmdAnalyze.getParamToCmd(cmd);
+                    cmd = cmdClass.getParamsCmd(cmd, String.valueOf(7));//getParamsCmd
+                    sendCmdCodeByHex(cmd);
+                }
             }
         });
     }
@@ -195,6 +342,7 @@ public class CoefficientsActivity extends MyBaseActivity2 {
         edtBxiasB.setText("");
         edtBxiasC.setText("");
         edtBxiasD.setText("");
+        clearUIState();
     }
 
     /**
@@ -235,6 +383,7 @@ public class CoefficientsActivity extends MyBaseActivity2 {
                 break;
         }
         cmd = cmdAnalyze.getParamToCmd(cmd);
+        Log.d("chenliang111", "String.valueOf(No - 1):" + String.valueOf(No - 1));
         cmd = cmdClass.getParamsCmd(cmd, String.valueOf(No - 1));//getParamsCmd
         sendCmdCodeByHex(cmd);
         coefficientsHandler.getDelaySendHandler().sendEmptyMessageDelayed(No + 1, DELAYTIME);
@@ -297,18 +446,154 @@ public class CoefficientsActivity extends MyBaseActivity2 {
 
         System.out.println("CoefficientsActivity接收到的数据:" + formatMsgContent(array));
         //返回的数据就删除延迟handle里面的消息
-        if (isRefresh) {
+        if (isRefresh == 1) {
             //设置
             coefficientsHandler.getDelaySendHandler().removeCallbacksAndMessages(null);
             String hexStr = Utils.ByteArraytoHex(array).replace(" ", "");
             parseVerifyData(hexStr);
-        } else {
+        } else if (isRefresh == 0) {
             //刷新数据
             String hexStr = Utils.ByteArraytoHex(array).replace(" ", "");
             if (hexStr.length() == 24) {
                 //加判断是为了避免其它命令接收数据造成这里解析出错
                 refreshVerifyData(hexStr.substring(6, 20), hexStr);
             }
+        } else if (isRefresh == 2) {
+            coefficientsHandler.getDelaySendHandler().removeCallbacksAndMessages(null);
+            String hexStr = Utils.ByteArraytoHex(array).replace(" ", "");
+            parseVerifyDataSingle(hexStr);
+        } else {
+            //nothing todo
+        }
+    }
+
+    private void parseVerifyDataSingle(String codeStr) {
+
+        switch (singleCount) {
+            case 1:
+                try {
+                    if (codeStr.substring(8, 10).equals("00")) {
+                        verifyDataBean.setAaxisA(edtAxiasA.getText().toString());
+                    } else {
+                        imgAxisA.setImageResource(R.drawable.error);
+                    }
+                } catch (Exception e) {
+                    imgAxisA.setImageResource(R.drawable.error);
+                }
+                imgAxisA.setImageResource(R.drawable.tick);
+                imgAxisA.setVisibility(View.VISIBLE);
+                Toast.makeText(CoefficientsActivity.this, "A axis A設置完畢", Toast.LENGTH_SHORT).show();
+                break;
+            case 2:
+                try {
+                    if (codeStr.substring(8, 10).equals("01")) {
+
+                        verifyDataBean.setAaxisB(edtAxiasB.getText().toString());
+                    } else {
+                        imgAxisB.setImageResource(R.drawable.error);
+                    }
+                } catch (Exception e) {
+                    imgAxisB.setImageResource(R.drawable.error);
+                }
+                imgAxisB.setImageResource(R.drawable.tick);
+                imgAxisB.setVisibility(View.VISIBLE);
+                Toast.makeText(CoefficientsActivity.this, "A axis B設置完畢", Toast.LENGTH_SHORT).show();
+                break;
+            case 3:
+                try {
+                    if (codeStr.substring(8, 10).equals("02")) {
+
+                        verifyDataBean.setAaxisC(edtAxiasC.getText().toString());
+                    } else {
+                        imgAxisC.setImageResource(R.drawable.error);
+                    }
+                } catch (Exception e) {
+                    imgAxisC.setImageResource(R.drawable.error);
+                }
+                imgAxisC.setImageResource(R.drawable.tick);
+                imgAxisC.setVisibility(View.VISIBLE);
+                Toast.makeText(CoefficientsActivity.this, "A axis C設置完畢", Toast.LENGTH_SHORT).show();
+                break;
+            case 4:
+
+                try {
+                    if (codeStr.substring(8, 10).equals("03")) {
+                        verifyDataBean.setAaxisD(edtAxiasD.getText().toString());
+                    } else {
+                        imgAxisD.setImageResource(R.drawable.error);
+                    }
+                } catch (Exception e) {
+                    imgAxisD.setImageResource(R.drawable.error);
+                }
+                imgAxisD.setImageResource(R.drawable.tick);
+                imgAxisD.setVisibility(View.VISIBLE);
+                Toast.makeText(CoefficientsActivity.this, "A axis D設置完畢", Toast.LENGTH_SHORT).show();
+                break;
+            case 5:
+
+                try {
+                    if (codeStr.substring(8, 10).equals("04")) {
+                        verifyDataBean.setBaxisA(edtBxiasA.getText().toString());
+                    } else {
+                        imgBxisA.setImageResource(R.drawable.error);
+                    }
+                } catch (Exception e) {
+                    imgBxisA.setImageResource(R.drawable.error);
+                }
+                imgBxisA.setImageResource(R.drawable.tick);
+                imgBxisA.setVisibility(View.VISIBLE);
+                Toast.makeText(CoefficientsActivity.this, "B axis A設置完畢", Toast.LENGTH_SHORT).show();
+                break;
+            case 6:
+                try {
+                    if (codeStr.substring(8, 10).equals("05")) {
+
+                        verifyDataBean.setBaxisB(edtBxiasB.getText().toString());
+                    } else {
+                        imgBxisB.setImageResource(R.drawable.error);
+                    }
+                } catch (Exception e) {
+                    imgBxisB.setImageResource(R.drawable.error);
+                }
+                imgBxisB.setImageResource(R.drawable.tick);
+                imgBxisB.setVisibility(View.VISIBLE);
+                Toast.makeText(CoefficientsActivity.this, "B axis B設置完畢", Toast.LENGTH_SHORT).show();
+                break;
+            case 7:
+
+                try {
+                    if (codeStr.substring(8, 10).equals("06")) {
+                        verifyDataBean.setBaxisC(edtBxiasC.getText().toString());
+                    } else {
+                        imgBxisC.setImageResource(R.drawable.error);
+                    }
+                } catch (Exception e) {
+                    imgBxisC.setImageResource(R.drawable.error);
+                }
+                imgBxisC.setImageResource(R.drawable.tick);
+                imgBxisC.setVisibility(View.VISIBLE);
+                Toast.makeText(CoefficientsActivity.this, "B axis C設置完畢", Toast.LENGTH_SHORT).show();
+                break;
+            case 8:
+
+                try {
+                    if (codeStr.substring(8, 10).equals("07")) {
+
+                        verifyDataBean.setBaxisD(edtBxiasD.getText().toString());
+                    } else {
+                        imgBxisD.setImageResource(R.drawable.error);
+                    }
+                    myApplication.setVerifyDataBean(verifyDataBean);
+                } catch (Exception e) {
+                    imgBxisD.setImageResource(R.drawable.error);
+                }
+                imgBxisD.setImageResource(R.drawable.tick);
+                imgBxisD.setVisibility(View.VISIBLE);
+                Toast.makeText(CoefficientsActivity.this, "B axis D設置完畢", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+
+                break;
         }
     }
 
@@ -477,7 +762,13 @@ public class CoefficientsActivity extends MyBaseActivity2 {
         @Override
         public void handleMessage(@NonNull Message msg) {
             super.handleMessage(msg);
-            sendConvertCmd(msg.what);
+            if (msg.what == 9) {
+//                verifyLoadDialog.dismiss();
+                btnSet.setEnabled(true);
+                Toast.makeText(CoefficientsActivity.this, "全部設置完畢", Toast.LENGTH_SHORT).show();
+            } else {
+                sendConvertCmd(msg.what);
+            }
             //伴随着有个500ms的超时发送
         }
 
@@ -504,6 +795,8 @@ public class CoefficientsActivity extends MyBaseActivity2 {
             super.handleMessage(msg);
             if (msg.what == 9) {
 //                verifyLoadDialog.dismiss();
+//                Log.d("chenliang111",)
+                btnRefresh.setEnabled(true);
                 Toast.makeText(CoefficientsActivity.this, "Refresh Success", Toast.LENGTH_SHORT).show();
             } else {
                 sendCmdGetVerifyCode(msg.what);
@@ -556,6 +849,7 @@ public class CoefficientsActivity extends MyBaseActivity2 {
                 edtBxiasD.setText(d);
                 myApplication.setVerifyDataBean(verifyDataBean);
                 refreshCount = 0;
+                btnRefresh.setEnabled(true);
                 Toast.makeText(CoefficientsActivity.this, "刷新成功！", Toast.LENGTH_SHORT).show();
                 break;
             default:
